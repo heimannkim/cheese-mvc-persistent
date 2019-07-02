e00dede22fc2ace7f2ff90cd41ca0a4f805dbbf6
Kim Heimann <heimannk@charter.net>
2019-07-01 19:15:30 -0500

    Final commit after steps 2 & 3.  Working!!!

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -50,23 +49,6 @@ public class CategoryController {
         }
 
         categoryDao.save(newCategory);
-        return "redirect:";
-    }
-
-    @RequestMapping(value = "remove", method = RequestMethod.GET)
-    public String displayRemoveCategoryForm(Model model) {
-        model.addAttribute("categories", categoryDao.findAll());
-        model.addAttribute("title", "Remove Category");
-        return "category/remove";
-    }
-
-    @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String processRemoveCategoryForm(@RequestParam int[] categoryIds) {
-
-        for (int categoryId : categoryIds) {
-            categoryDao.delete(categoryId);
-        }
-
         return "redirect:";
     }
 
